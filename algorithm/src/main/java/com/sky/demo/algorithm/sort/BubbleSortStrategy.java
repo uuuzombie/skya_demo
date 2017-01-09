2151be18b445c99c45b246aac3f0e9c92fe38eba
rg <uuuzombie@gmail.com>
2017-01-09 22:59:02 +0800

    update bubble sort

--- algorithm/src/main/java/com/sky/demo/algorithm/sort/BubbleSortStrategy.java
+++ algorithm/src/main/java/com/sky/demo/algorithm/sort/BubbleSortStrategy.java
@@ -11,8 +11,8 @@ public class BubbleSortStrategy extends SortStrategy {
     public void sort(int[] array) {
         Preconditions.checkNotNull(array, "array is null!");
 
-        for (int i = 0; i < array.length - 1; i++) {
-            for (int j = 0; j < array.length - i - 1; j++) {
+        for (int i = 0; i < array.length - 1; i++) {            //只需遍历n-1次
+            for (int j = 0; j < array.length - i - 1; j++) {    //从上向下遍历
                 if (array[j] > array[j + 1]) {
                     Integer temp = array[j];
                     array[j] = array[j + 1];
