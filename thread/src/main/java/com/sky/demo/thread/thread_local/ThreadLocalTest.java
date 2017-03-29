ba951a22465dd48eda8a780e38097a7c2cd3a2a6
rg <uuuzombie@gmail.com>
2017-03-29 13:50:39 +0800

    update threadLocalTest

--- thread/src/main/java/com/sky/demo/thread/thread_local/ThreadLocalTest.java
+++ thread/src/main/java/com/sky/demo/thread/thread_local/ThreadLocalTest.java
@@ -5,24 +5,27 @@ package com.sky.demo.thread.thread_local;
  */
 public class ThreadLocalTest {
 
-//    public static ThreadLocal<String> QT = new InheritableThreadLocal<String>();
+//    public static String QT = null;
 //    public static ThreadLocal<String> QT = new ThreadLocal<String>();
-    public static String QT = null;
+    public static ThreadLocal<String> QT = new InheritableThreadLocal<String>();
 
     public static void main(String[] args) {
+
+        QT.set("aaa");      //子线程会继承父线程的变量
 
         int count = 1000;
 
         for (int i = 0; i < count; i++) {
             final int finalI = i;
+
             new Thread(new Runnable() {
                 @Override
                 public void run() {
-                    QT = String.valueOf(finalI);
-                    System.out.println(Thread.currentThread().getName() + ":" + QT);
+//                    QT = String.valueOf(finalI);
+//                    System.out.println(Thread.currentThread().getName() + ":" + QT);
 
-//                    QT.set(str);
-//                    System.out.println(Thread.currentThread().getName() + ":" + QT.get());
+//                    QT.set(String.valueOf(finalI));
+                    System.out.println(Thread.currentThread().getName() + ":" + QT.get());
                 }
             }).start();
         }
